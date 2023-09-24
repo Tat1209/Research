b0eb245ba9be6f72137e1e0c5d65748df0a30a4a
Tat1209 <taikunn12740pqp@icloud.com>
2023-09-24 23:40:15 +0900

    feat: 訓練しやすい構造にした。たぶん。

--- app/ee/learn_woflow.py
+++ app/ee/learn_woflow.py
@@ -17,47 +17,52 @@ save_path = "/home/haselab/Documents/tat/app/ee/results/"
 learning_rate = 0.0001  
 batch_size = 500        
 
-nb_fils = 16
-epochs = 500
+nb_fils = 4
+
+fil_times = 6
 times = 1 
+epochs = 500
 
-run_name = f"r18_{nb_fils}f"
 
-try: df = pl.read_csv(f"{save_path}{run_name}_ntrain.csv")
-except: df = pl.DataFrame()
+for fi in range(fil_times):
+    fils = nb_fils * 2 ** fi
+    run_name = f"r18_{fils}f"
 
-for ni in range(times):
+    try: df = pl.read_csv(f"{save_path}{run_name}_ntrain.csv")
+    except: df = pl.DataFrame()
 
-    network = net(nb_fils=nb_fils, num_classes=100)
-    loss_func = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(network.parameters(), lr=learning_rate)
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs, eta_min=0, last_epoch=-1)
-    model = Model(network, loss_func, device=device, optimizer=optimizer, scheduler=scheduler)
+    for ni in range(times):
+        print(f'{fils}filters')
+        network = net(nb_fils=fils, num_classes=100)
+        loss_func = torch.nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(network.parameters(), lr=learning_rate)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs, eta_min=0, last_epoch=-1)
+        model = Model(network, loss_func, device=device, optimizer=optimizer, scheduler=scheduler)
 
-    print(f'train {ni+1}/{times}')
+        print(f'train {ni+1}/{times}')
 
-    for e in range(epochs):
-        Loss, Acc = model.train_1epoch(pr.dl("cifar_train", tr.cf_crop, batch_size), mixup=True)
-        vLoss, vAcc = model.val_1epoch(pr.dl("cifar_val", tr.cf_gen, batch_size))
+        for e in range(epochs):
+            Loss, Acc = model.train_1epoch(pr.dl("cifar_train", tr.cf_crop, batch_size), mixup=True)
+            vLoss, vAcc = model.val_1epoch(pr.dl("cifar_val", tr.cf_gen, batch_size))
 
-        met_dict = {"epoch":e+1, "Loss":Loss, "Acc":Acc, "vLoss":vLoss, "vAcc":vAcc}
-        model.log_met(met_dict)
-        model.printlog(met_dict, e, epochs, itv=5)
-        # model.printlog(met_dict, e, epochs, itv=epochs)
+            met_dict = {"epoch":e+1, "Loss":Loss, "Acc":Acc, "vLoss":vLoss, "vAcc":vAcc}
+            model.log_met(met_dict)
+            model.printlog(met_dict, e, epochs, itv=5)
+            # model.printlog(met_dict, e, epochs, itv=epochs)
 
-    # 初回だけ保存
-    if ni == 0: model.hist_to_csv(f"{save_path}{run_name}_1train.csv")
-    
-    # metrix保存 dfには学習後metrixが試行ごとに記録される
-    # if ni == 0: df = model.get_last_met()
-    # else: df = pl.concat([df, model.get_last_met()])
-    df = pl.concat([df, model.get_last_met()])
-    
-    
-df.write_csv(f"{save_path}{run_name}_ntrain.csv")
-df_stat = df.describe()
-print(df_stat)
-df_stat.write_csv(f"{save_path}{run_name}_stat.csv")
+        # 初回だけ保存
+        if ni == 0: model.hist_to_csv(f"{save_path}{run_name}_1train.csv")
+        
+        # metrix保存 dfには学習後metrixが試行ごとに記録される
+        # if ni == 0: df = model.get_last_met()
+        # else: df = pl.concat([df, model.get_last_met()])
+        df = pl.concat([df, model.get_last_met()])
+        
+        
+    df.write_csv(f"{save_path}{run_name}_ntrain.csv")
+    df_stat = df.describe()
+    print(df_stat)
+    df_stat.write_csv(f"{save_path}{run_name}_stat.csv")
 
 
 # model.save_ckpt(f"{save_path}{run_name}.ckpt")
